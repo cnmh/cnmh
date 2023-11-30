765d40e5f3b7b0cdc6dc6b57de6f9e6e054437a1
Reda Grain <grain.reda.solicode@gmail.com>
2023-11-30 15:33:28 +0100

    Remove controller

--- resources/views/type_handicaps/table.blade.php
+++ resources/views/type_handicaps/table.blade.php
@@ -27,40 +27,38 @@
     <div class="table-responsive">
         <table class="table table-striped" id="type-handicaps-table">
             <thead>
-            <tr>
-                <th>Nom</th>
-                <th>Description</th>
-                <th colspan="3">Action</th>
-            </tr>
+                <tr>
+                    <th>Nom</th>
+                    <th>Description</th>
+                    <th colspan="3">Action</th>
+                </tr>
             </thead>
             <tbody>
-            @foreach($typeHandicaps as $typeHandicap)
+                @foreach($typeHandicaps as $typeHandicap)
                 <tr>
                     <td>{{ $typeHandicap->nom }}</td>
                     <td>{!! $typeHandicap->description !!}</td>
-                    <td  style="width: 120px">
+                    <td style="width: 120px">
                         {!! Form::open(['route' => ['typeHandicaps.destroy', $typeHandicap->id], 'method' => 'delete']) !!}
                         <div class='btn-group'>
-                            @can('show-TypeHandicapController')
-                            <a href="{{ route('typeHandicaps.show', [$typeHandicap->id]) }}"
-                               class='btn btn-default btn-sm'>
+                            @can('show-TypeHandicap')
+                            <a href="{{ route('typeHandicaps.show', [$typeHandicap->id]) }}" class='btn btn-default btn-sm'>
                                 <i class="far fa-eye"></i>
                             </a>
                             @endcan
-                            @can('edit-TypeHandicapController')
-                            <a href="{{ route('typeHandicaps.edit', [$typeHandicap->id]) }}"
-                               class='btn btn-default btn-sm'>
+                            @can('edit-TypeHandicap')
+                            <a href="{{ route('typeHandicaps.edit', [$typeHandicap->id]) }}" class='btn btn-default btn-sm'>
                                 <i class="far fa-edit"></i>
                             </a>
                             @endcan
-                            @can('destroy-TypeHandicapController')
-                              {!! Form::button('<i class="far fa-trash-alt"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                            @can('destroy-TypeHandicap')
+                            {!! Form::button('<i class="far fa-trash-alt"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
                         </div>
                         {!! Form::close() !!}
                         @endcan
                     </td>
                 </tr>
-            @endforeach
+                @endforeach
             </tbody>
         </table>
     </div>
@@ -70,13 +68,13 @@
             @include('adminlte-templates::common.paginate', ['records' => $typeHandicaps])
         </div>
         <div class="float-left">
-            @can('export-TypeHandicapController')
+            @can('export-TypeHandicap')
             <a href="{{ route('typehandicap.export') }}" class="btn btn-default swalDefaultQuestion">
                 <i class="fas fa-download"></i> Exporter
             </a>
             @endcan
-            @can('import-TypeHandicapController')
-            <button  class="btn btn-default swalDefaultQuestion" data-toggle="modal" data-target="#importModel">
+            @can('import-TypeHandicap')
+            <button class="btn btn-default swalDefaultQuestion" data-toggle="modal" data-target="#importModel">
                 <i class="fas fa-file-import"></i> Importer
             </button>
             @endcan
@@ -84,8 +82,7 @@
     </div>
 </div>
 <!-- Modal Import -->
-<div class="modal fade" id="importModel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle"
-    aria-hidden="true">
+<div class="modal fade" id="importModel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -109,4 +106,3 @@
     </div>
 </div>
 {{-- end Model --}}
-
