98d74e1a835c0df6c28e2cc1e7012e8d040a4a44
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2023-12-01 16:51:39 +0100

    update menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -20,13 +20,13 @@
     </a>
     <ul class="nav nav-treeview" style="">
         <li class="nav-item">
-            <a href="/consultations/liste-attente" class="nav-link {{ Route::is('users.index' . '*') ? 'active' : '' }}">
+            <a href="/consultations/liste-attente" class="nav-link {{ Route::is('consultations.index' . '*') ? 'active' : '' }}">
                 <p>Liste de attente </p>
             </a>
         </li>
 
         <li class="nav-item">
-            <a href="{{ route('rendez-vous.index') }}" class="nav-link {{ Route::is('users.index' . '*') ? 'active' : '' }}">
+            <a href="{{ route('rendez-vous.index') }}" class="nav-link {{ Route::is('rendez-vous.index' . '*') ? 'active' : '' }}">
                 <p>Rendez-vous </p>
             </a>
         </li>
