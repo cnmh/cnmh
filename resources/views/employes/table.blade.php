35e0f7b007622324e0d10931fe8bf0fec2fd4ae1
jalil betroji <jalil.betroji@gmail.com>
2023-12-02 15:49:55 +0100

    -Employes- permissions have been updated for administrator

--- resources/views/employes/table.blade.php
+++ resources/views/employes/table.blade.php
@@ -38,9 +38,9 @@
                             @endcan
                             @can('destroy-Employe')
                             {!! Form::button('<i class="far fa-trash-alt"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                            @endcan
                         </div>
                         {!! Form::close() !!}
-                        @endcan
                     </td>
                 </tr>
             @endforeach
