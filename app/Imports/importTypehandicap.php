559aa4478dbd71b41e465081a1cb977f9b3d3ef9
boukharsoufiane1998 <boukhar.soufiane.coding@gmail.com>
2023-12-05 16:25:10 +0100

    fix import typeHandicap and view

--- app/Imports/importTypehandicap.php
+++ app/Imports/importTypehandicap.php
@@ -4,19 +4,31 @@ namespace App\Imports;
 
 use App\Models\TypeHandicap;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithStartRow;
 
-class importTypehandicap implements ToModel
+
+class importTypehandicap implements ToModel,WithStartRow
 {
     /**
     * @param array $row
     *
     * @return \Illuminate\Database\Eloquent\Model|null
     */
+
+    public $headingRow = null;
+
     public function model(array $row)
     {
-        return new TypeHandicap([
-            'nom' => $row[1],
-            'description' => $row[2],
-        ]);
+       $typeHandicap = TypeHandicap::firstOrNew(['nom' => $row[0]]);
+
+       $typeHandicap->description = $row[1];
+
+       $typeHandicap->save();
+
+       return $typeHandicap;
+    }
+
+    public function startRow():int{
+        return 2;
     }
 }
