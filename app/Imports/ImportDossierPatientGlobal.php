da161991d2bf18d58d64cdd7c98f9823af3bbf53
boukharsoufiane1998 <boukhar.soufiane.coding@gmail.com>
2023-12-05 20:47:45 +0100

    save import dossier

--- app/Imports/ImportDossierPatientGlobal.php
+++ app/Imports/ImportDossierPatientGlobal.php
@@ -5,8 +5,10 @@ namespace App\Imports;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
-use App\Imports\ImportTuteur;
-use App\Imports\ImportPatient;
+use Maatwebsite\Excel\Concerns\WithStartRow;
+
+use App\Models\Tuteur;
+use App\Models\Patient;
 
 class ImportDossierPatientGlobal implements WithMultipleSheets
 {
@@ -14,14 +16,102 @@ class ImportDossierPatientGlobal implements WithMultipleSheets
 
     public function sheets(): array
     {
+        $importTuteur = new ImportTuteur();
+        $importPatient = new ImportPatient();
+
         return [
-            'Tuteur' => new ImportTuteur($this),
-            'Patient' => new ImportPatient($this->tuteurId),
+            'Tuteur' => $importTuteur,
+            'Patient' => $importPatient,
         ];
     }
 
     public function setTuteurId($tuteurId)
     {
         $this->tuteurId = $tuteurId;
+    }
+}
+
+class ImportTuteur implements ToCollection,WithStartRow
+{
+    public $headingRow = null;
+
+    public function collection(Collection $rows)
+    {
+        foreach ($rows as $row) {
+            $emailTuteur = $row[5];
+            $tuteur = Tuteur::where('email', $emailTuteur)->first();
+
+            if (!$tuteur) {
+                $tuteur = new Tuteur;
+                $tuteur->etat_civil_id = $row[0];
+                $tuteur->nom = $row[1];
+                $tuteur->prenom = $row[2];
+                $tuteur->sexe = $row[3];
+                $tuteur->telephone = $row[4];
+                $tuteur->email = $row[5];
+                $tuteur->adresse = $row[6];
+                $tuteur->cin = $row[7];
+                $tuteur->remarques = $row[8];
+
+                if (!$tuteur->save()) {
+                    $errors = $tuteur->getErrors();
+                }
+            }
+
+            if (!is_null($tuteur->id)) {
+                $importPatient = new ImportPatient();
+                $importPatient->setTuteurId($tuteur->id);
+            }
+        }
+    }
+
+    public function startRow(): int
+    {
+        return 2;
+    }
+}
+
+class ImportPatient implements ToCollection,WithStartRow
+{
+    private $tuteurId;
+
+    public function setTuteurId($tuteurId)
+    {
+        $this->tuteurId = $tuteurId;
+    }
+
+    public $headingRow = null;
+
+    public function collection(Collection $rows)
+    {
+        foreach ($rows as $row) {
+            $namePatient = $row[1];
+            $prenomPatient = $row[2];
+
+            $patient = Patient::where('nom', $namePatient)->where('prenom', $prenomPatient)->first();
+
+            if (!$patient) {
+                $patient = new Patient;
+                $patient->tuteur_id = $this->tuteurId;
+                $patient->niveau_scolaire_id = $row[0];
+                $patient->nom = $row[1];
+                $patient->prenom = $row[2];
+                $patient->telephone = $row[3];
+                $patient->cin = $row[4];
+                $patient->email = $row[5];
+                $patient->image = null;
+                $patient->adresse = $row[6];
+                $patient->remarques = $row[7];
+
+                if (!$patient->save()) {
+                    $errors = $patient->getErrors();
+                }
+            }
+        }
+    }
+
+     public function startRow(): int
+    {
+        return 2;
     }
 }
