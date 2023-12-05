80d6f5ca448e77cf57a68f33da96a86d598f5812
boukharsoufiane1998 <boukhar.soufiane.coding@gmail.com>
2023-12-05 14:43:05 +0100

    fix authentification

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -36,5 +38,28 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function validateLogin(Request $request)
+    {
+        $request->validate([
+            $this->username() => 'required|string',
+            'password' => 'required|string',
+        ], $this->customValidationMessages());
+    }
+
+    protected function customValidationMessages()
+    {
+        return [
+            $this->username() . '.required' => 'Le champ Email est requis.',
+            'password.required' => 'Le champ Mot de passe est requis.',
+        ];
+    }
+
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        throw ValidationException::withMessages([
+            $this->username() => [trans('auth.failed')],
+        ])->status(session(['status' => trans('auth.failed')]));
     }
 }
