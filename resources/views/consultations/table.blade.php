04fce93f304d65a18c88aa6ac5e280154ff06c86
jalil betroji <jalil.betroji@gmail.com>
2023-12-16 16:19:51 +0100

    destroy consultation button removed for -medecin générale-

--- resources/views/consultations/table.blade.php
+++ resources/views/consultations/table.blade.php
@@ -2,77 +2,113 @@
     <div class="table-responsive">
         <table class="table table-striped" id="consultations-table">
             <thead>
-            <tr>
-                <th>Nom</th>
-                <th>Prénom</th>
-                <th>Téléphone</th>
-                <th>Date enregistrement</th>
-                <th>État</th>
-                <th>Orientation</th>
-                <th>Date consultation</th>
-                <th colspan="3">Action</th>
-            </tr>
+                <tr>
+                    <th>Nom</th>
+                    <th>Prénom</th>
+                    <th>Téléphone</th>
+                    <th>Date enregistrement</th>
+                    <th>État</th>
+                    <th>Orientation</th>
+                    <th>Date consultation</th>
+                    <th colspan="3">Action</th>
+                </tr>
             </thead>
             <tbody>
-            @foreach($consultations as $consultation)
+                @foreach($consultations as $consultation)
                 <tr>
-                    <td>{{ $consultation->nom }}</td>
+                    <td>{{ $consultation->id }}</td>
                     <td>{{ $consultation->prenom }}</td>
                     <td>{{ $consultation->telephone }}</td>
                     <td>{{ $consultation->date_enregistrement }}</td>
                     <td>
                         @if($consultation->etat === 'enRendezVous')
-                          En rendez-vous
+                        En rendez-vous
                         @elseif($consultation->etat === 'enAttente')
-                          En attente
+                        En attente
                         @elseif($consultation->etat === 'enConsultation')
-                          En consultation
+                        En consultation
                         @endif
                     </td>
                     <td>
                         @if($consultation->type === 'medecinGeneral')
-                         Médecin générale
+                        Médecin générale
                         @endif
                     </td>
                     <td>{{ $consultation->date_consultation }}</td>
-                    <td  style="width: 120px">
-                        {!! Form::open(['route' => ['consultations.destroy', $consultation->id], 'method' => 'delete']) !!}
+                    <td style="width: 120px">
+                        {!! Form::open(['route' => ['consultations.destroy', $consultation->id], 'method' => 'delete'])
+                        !!}
                         <div class='btn-group'>
                             @can('show-Consultation')
-                            <a href="{{ route('consultations.show', [$title,$consultation->id]) }}"
-                               class='btn btn-default btn-sm'>
+                            <a href="{{ route('consultations.show', [$title,$consultation->consultation_id]) }}"
+                                class='btn btn-default btn-sm'>
                                 <i class="far fa-eye"></i>
                             </a>
                             @endcan
                             @can('edit-Consultation')
-                           <a href="{{ route('consultations.edit', [$consultation->id]) }}"
+                           <a href="{{ route('consultations.edit', [$consultation->consultation_id]) }}"
                                class='btn btn-default btn-sm'>
                                 <i class="far fa-edit"></i>
                             </a>
                             @endcan
                             @can('destroy-Consultation')
+                            {{-- 
                              {!! Form::button('<i class="far fa-trash-alt"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                            
+                            --}}
                              @endcan
                         </div>
                         {!! Form::close() !!}
                     </td>
                 </tr>
-            @endforeach
+
+                @endforeach
             </tbody>
         </table>
     </div>
+</div>
+<div class="card-footer clearfix">
+    <div class="float-left d-flex">
+        <form action="{{ route('consultations.export') }}" method="post">
+            @csrf 
+            <button type="submit" class="btn btn-default swalDefaultQuestion">
+                <i class="fas fa-download"></i> @lang('crud.export')
+            </button>
+        </form>
+        
+        <button class="btn btn-default swalDefaultQuestion" data-toggle="modal" data-target="#importModel">
+                <i class="fas fa-file-import"></i> @lang('crud.import')
+        </button>
+    </div>
+    <div class="float-right">
+        @include('adminlte-templates::common.paginate', ['records' => $consultations])
+    </div>
+</div>
 
-    <div class="card-footer clearfix">
-        <div class="float-right">
-            @include('adminlte-templates::common.paginate', ['records' => $consultations])
-        </div>
-        <div class="float-left">
-                                <button type="button" class="btn btn-default swalDefaultQuestion">
-                                    <i class="fas fa-download"></i> @lang('crud.export')
-                                </button>
-                                <button type="button" class="btn btn-default swalDefaultQuestion">
-                                    <i class="fas fa-file-import"></i> @lang('crud.import')
-                                </button>
+
+
+<!-- Modal Import -->
+<div class="modal fade" id="importModel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">@lang('crud.print') </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="{{ route('consultations.import') }}" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <input type="file" name="file" class="form-control">
+                    <br>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">@lang('crud.cancel')</button>
+                        <button class="btn btn-success">@lang('crud.import')</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
