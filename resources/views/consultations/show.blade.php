42af9cb7465705da8fbd99134712d8643a0c9b47
boukharSoufiane1998 <boukhar.soufiane.coding@gmail.com>
2023-12-19 13:36:12 +0100

    fix consultation list attent service social

--- resources/views/consultations/show.blade.php
+++ resources/views/consultations/show.blade.php
@@ -5,14 +5,14 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>
+                    <h5>
                         @lang('crud.details')
                         @if (app()->getLocale() == 'fr')
-                           de la {{ strtolower(__('models/consultations.singular')) }} {{$title}}
+                           de la {{ strtolower(__('models/consultations.singular')) }} Liste d'attente
                         @else
                             {{ strtolower(__('models/consultations.singular')) }}
                         @endif
-                    </h1>
+                    </h5>
                 </div>
                 <div class="col-sm-6">
                     <a class="btn btn-default float-right"
