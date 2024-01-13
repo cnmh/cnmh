c256870f372b5045f9416460132fa64c72043382
jalil betroji <jalil.betroji@gmail.com>
2024-01-13 14:26:33 +0100

    neveaux type handicap added

---
+++ database/seeders/Autorizations/Maintenance_1_1_5.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders\Autorizations;
+
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use App\Models\TypeHandicap;
+
+class Maintenance_1_1_3 extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $now = \Carbon\Carbon::now();
+
+        $typehandicap = TypeHandicap::insert([
+            [
+                'nom' => 'PC Épilepsie',
+                'description' => 'description type handicap 9',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+        ]);
+    
+    }
+}
