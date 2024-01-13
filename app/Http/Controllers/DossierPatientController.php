ccf8ce8b094a0e67418f0e59374504ddfcb89995
Boukhar <boukhar@kali.boukhar>
2024-01-13 08:17:16 -0500

    update tuteur search par cin

--- app/Http/Controllers/DossierPatientController.php
+++ app/Http/Controllers/DossierPatientController.php
@@ -414,7 +414,7 @@ class DossierPatientController extends AppBaseController
                 $tuteurs = Tuteur::join('etat_civils', 'tuteurs.etat_civil_id', '=', 'etat_civils.id')
                 ->where('tuteurs.nom', 'like', '%' . $search . '%')
                 ->orWhere('tuteurs.prenom', 'like', '%' . $search . '%')
-                ->orWhere('tuteurs.cni', 'like', '%' . $search . '%')
+                ->orWhere('tuteurs.cin', 'like', '%' . $search . '%')
                 ->select('tuteurs.nom as tuteur_nom', 'etat_civils.id as etat_civil_id', 'tuteurs.prenom', 'tuteurs.adresse', 'tuteurs.telephone', 'tuteurs.email', 'etat_civils.nom as etat_civil_nom','tuteurs.id as id')
                 ->paginate();
     
