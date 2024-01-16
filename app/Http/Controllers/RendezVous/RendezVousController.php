4d2b88dd73d97cfa2e0807e7955583c45bdc2616
boukharSoufiane1998 <boukhar.soufiane.coding@gmail.com>
2024-01-16 11:19:51 +0100

    save travail

--- app/Http/Controllers/RendezVous/RendezVousController.php
+++ app/Http/Controllers/RendezVous/RendezVousController.php
@@ -9,7 +9,8 @@ use App\Models\Consultation;
 use App\Models\DossierPatientConsultation;
 use App\Models\RendezVous;
 use App\Models\DossierPatient;
-use App\Repositories\RendezVousRepository;
+use App\Repositories\RendezVous\RendezVousRepository;
+use App\Repositories\EntretienSocial\DossierPatientRepository;
 use Illuminate\Http\Request;
 use Flash;
 use Illuminate\Support\Facades\Auth;
@@ -27,7 +28,26 @@ class RendezVousController extends AppBaseController
 
     public function list_rendezVous(Request $request)
     {
-       
+        $query = $request->input('query');
+        $rendezVouses = $this->rendezVousRepository->paginate($query);
+        foreach ($rendezVouses as $rendezVous) {
+            $consultation_id = $rendezVous->consultation_id;
+            $dossierPatientConsultationRepo = new DossierPatientConsultationRepository;
+            $dossierConsultation = $dossierPatientRepo->get();
+            if ($dossierConsultation) {
+                $dossier_patient_id = $dossierConsultation->dossier_patient_id;
+                $dossierPatientRepo = new DossierPatientRepository;
+                $dossierPatient = $dossierPatientRepo->find($dossier_patient_id);
+                if ($dossierPatient) {
+                    $dossier_numero = $dossierPatient->numero_dossier;
+                    $rendezVous->numero_dossier = $dossier_numero;
+                }
+            }
+        }
+        if ($request->ajax()) {
+            return view('rendez_vouses.table')->with('rendezVouses', $rendezVouses);
+        }
+        return view('rendez_vouses.index')->with('rendezVouses', $rendezVouses);
     }
 
     /**
@@ -40,8 +60,10 @@ class RendezVousController extends AppBaseController
         return view('rendez_vouses.list_dossier_patient',compact("dossier_patients"));
     }
 
-
-    public function FormAjouterRendezVous(Request $request){
+     /**
+     * Phase 2 : Create rendez vous
+     */
+    public function FormAjouterRendezVous(Request $request){;
         $consultation_id = $request->consultation_id;
         if(empty($consultation_id)){
             return back();
@@ -55,10 +77,12 @@ class RendezVousController extends AppBaseController
             'consultation_id' => 'required|exists:consultations,id',
             'remarques' => 'nullable|string',
         ]);
-        $rendezVousRepository->create($request->all());
+        $this->rendezVousRepository->create($request->all());
         Flash::success(__('messages.saved', ['model' => __('models/rendezVouses.singular')]));
         return redirect(route('rendez-vous.index'));
     }
+
+
 
 
 }
