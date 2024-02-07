77a71b02cb780e05700e723e59af66b1fa36afed
boukharSoufiane1998 <boukhar.soufiane.coding@gmail.com>
2024-02-07 10:56:51 +0100

    start travail

---
+++ database/seeders/Autorizations/Maintenance_1_2_3.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders\Autorizations;
+use Illuminate\Database\Seeder;
+
+class Maintenance_1_2_3 extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+    }
+}
