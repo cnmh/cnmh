584c7c9f52d7234b9fcc5cb7680f5a4b428caa2e
ESSARRAJ <essarraj.fouad@gmail.com>
2024-05-03 20:05:16 +0100

    save

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {   
+        // https://stackoverflow.com/questions/42244541/laravel-migration-error-syntax-error-or-access-violation-1071-specified-key-wa
         // TODO à ajouter dans laravel starter aprés savoir ce qu'il fait
         Schema::defaultStringLength(191);
 
