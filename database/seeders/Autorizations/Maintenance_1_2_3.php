a49f9194c9b9cc9ad2957cbb0008890598cdfa1e
Soufiane-Boukhar <boukhar.soufiane.coding@gmail.com>
2024-06-10 08:44:00 -0400

    update work

--- database/seeders/Autorizations/Maintenance_1_2_3.php
+++ database/seeders/Autorizations/Maintenance_1_2_3.php
@@ -11,9 +11,9 @@ class Maintenance_1_2_3 extends Seeder
      */
     public function run(): void
     {
-        $couvertureMedical = CouvertureMedical::where('nom','Ne sait pas')->first();
-        $couvertureMedical->update([
-            'nom' => 'Aucune'
-        ]);
+        // $couvertureMedical = CouvertureMedical::where('nom','Ne sait pas')->first();
+        // $couvertureMedical->update([
+        //     'nom' => 'Aucune'
+        // ]);
     }
 }
